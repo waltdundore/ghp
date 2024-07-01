2b427003f1c89fa4de21e6129f0339afce43a535
waltdundore <walt@dundore.org>
2024-07-01 15:24:09 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,9 +2,9 @@
 
 sudo dnf -y install git
 sudo dnf -y install dnf-plugins-core
-sudo dnf config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
+sudo dnf -y config-manager --add-repo https://download.docker.com/linux/fedora/docker-ce.repo
 
-sudo dnf install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+sudo dnf -y install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl enable docker --now
 sudo usermod -aG docker $USER
